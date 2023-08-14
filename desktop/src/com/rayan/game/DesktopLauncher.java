22cedbbbcdf448fe0a6a491a642b71982119cc8c
Rayan420 <ahmed.rayan@st.uskudar.edu.tr>
2023-08-15 01:10:02 +0300

    Initial commit

---
+++ desktop/src/com/rayan/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.rayan.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.rayan.game.FlappBird;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("flappy-bird-game");
+		new Lwjgl3Application(new FlappBird(), config);
+	}
+}
